9d2d0f86d40bf2b2cbc062d59491b42d328b2fdf
rmpakbagong <rmpakbagong@gmail.com>
2022-08-07 12:59:48 +0800

    text-animasi

--- js/animation.js
+++ js/animation.js
@@ -1,2 +1,6 @@
 gsap.registerPlugin(TextPlugin, ScrollTrigger);
 gsap.to("h1", { duration: 3, text: "EF - Studio" });
+gsap.to("p1", {
+  duration: 100,
+  text: "Born and raised in Bali, Epen Flow is Indonesian artist, illustrator & art director who translates his inspirations into visual narrative. His academic studies in Information System and fine art, along with his work experience in brand advertising and Design Expert with more than 8 years of expertise creating impactful visual brand solution for high - profile client across numeros industry vertical. along with his work experience have influenced his current artistic style. He loves to play with color and texture and focuses on visual subtlety. he pairs a broad range of creative leadership experience in brand strategy and development, graphic design, and B2B/B2C marketing. with a deep understanding of the psychology behind design to create effective campaigns and coporate narratives",
+});
