55cbd97808aeb68e56744ef69e80e16617f059f0
epenflow <epenflow@gmail.com>
2023-10-03 23:18:16 +0800

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import { DEFAULT_TEXT } from '@/constants/text';
 import GlobalStyles from '@/styles/GlobalStyles';
 import './global.css';
-export const metadata = {
+import { Metadata } from 'next';
+import EFLogoSVG from '/public/EFLogoSVG.svg';
+export const metadata: Metadata = {
 	title: 'EPEN FLOW',
 	description: DEFAULT_TEXT.description,
+	themeColor: 'black',
+	icons: EFLogoSVG.src,
 };
 
 export default async function RootLayout({
