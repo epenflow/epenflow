36de7df7ff7190c8b6c11383fae4c1209cb07b93
epenflow <epenflow@gmail.com>
2023-10-04 19:53:20 +0800

    adding animations

--- src/app/works/chat/page.tsx
+++ src/app/works/chat/page.tsx
@@ -4,10 +4,25 @@ import React, { ChangeEvent } from 'react';
 import gsap from 'gsap';
 import { TextPlugin } from 'gsap/TextPlugin';
 import { useRouter } from 'next/navigation';
+import Footer from '@/components/Footer';
+import Cakra from '@/components/Cakra';
+import Image from 'next/image';
+import epen from '/public/epen.png';
 gsap.registerPlugin(TextPlugin);
+type TWhereMouse = {
+	x: number;
+	y: number;
+};
 export default function Page() {
 	const [isChange, setChange] = React.useState<string>('');
 	const welcomeRef = React.useRef<HTMLHeadingElement>(null);
+	const terminalRef = React.useRef<HTMLDivElement>(null);
+	const [whereMouse, setWhereMoues] = React.useState<TWhereMouse>({
+		x: 0,
+		y: 0,
+	});
+	const previusRef = React.useRef<HTMLSpanElement>(null);
+	const hoverRef = React.useRef<HTMLDivElement>(null);
 	const router = useRouter();
 	const handleChange = (el: ChangeEvent<HTMLInputElement>) => {
 		setChange((prev) => (prev = el.target.value));
@@ -20,63 +35,167 @@ export default function Page() {
 			);
 		}
 	};
+	const handleClick = () => {
+		router.push(
+			`https://api.whatsapp.com/send/?phone=%2B62895330148034&text=${isChange}&type=phone_number&app_absent=0`
+		);
+	};
+	const handleMouseMove = (event: MouseEvent) => {
+		setWhereMoues((prev) => ({
+			...prev,
+			x: event.clientX,
+			y: event.clientY,
+		}));
+	};
 	React.useLayoutEffect(() => {
-		const ctx = gsap.context(() => {
-			gsap.from(welcomeRef.current, {
-				text: '',
+		const tl = gsap.timeline({
+			defaults: {
 				duration: 5,
-			});
+			},
+		});
+		const ctx = gsap.context(() => {
+			tl.from(
+				terminalRef.current,
+				{
+					clipPath: 'inset(0% 50% 0% 50%)',
+					yPercent: 100,
+					scale: 0,
+				},
+				1
+			).from(
+				welcomeRef.current,
+				{
+					text: '',
+					duration: 5,
+					delay: 4.5,
+				},
+				2
+			);
+		});
+		const hoverAnimations = gsap.fromTo(
+			hoverRef.current,
+			{
+				opacity: 0,
+				scale: 0,
+				yPercent: -100,
+				stagger: {
+					amount: 1,
+					each: 1,
+				},
+				visibility: 'none',
+			},
+			{
+				opacity: 1,
+				paused: true,
+				scale: 1,
+				yPercent: 0,
+				visibility: 'block',
+			}
+		);
+		previusRef.current?.addEventListener('mouseenter', () => {
+			hoverAnimations.play();
+		});
+		previusRef.current?.addEventListener('mouseleave', () => {
+			hoverAnimations.reverse();
 		});
 		return () => {
 			ctx.revert();
 		};
 	}, []);
+	React.useEffect(() => {
+		window.addEventListener('mousemove', handleMouseMove);
 
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+		};
+	}, []);
 	return (
-		<main className='flex justify-center items-center h-screen max-h-screen p-4 flex-col gap-3'>
-			<div className='bg-blue-600 w-full h-3/4 md:h-1/2 md:w-1/2 relative rounded-lg border-solid border-2 border-[#39FF14] box-content overflow-y-auto'>
-				<div className='absolute bg-white top-0 h-7 items-center flex flex-row gap-1  shadow-lg w-full rounded-t-md border-b-2 border-solid border-[#39FF14]'>
-					<Link href={'/works'}>
-						<span className='h-3 w-3 block bg-red-500 rounded-full ml-1' />
-					</Link>
-					<span className='h-3 w-3 block bg-yellow-500 rounded-full ml-1' />
-					<span className='h-3 w-3 block bg-green-500 rounded-full ml-1' />
+		<React.Fragment>
+			<main className='flex justify-center items-center h-screen max-h-screen p-4 flex-col gap-3 overflow-y-hidden'>
+				<div
+					className='bg-blue-600 h-52 w-52 z-50 hidden md:flex sm:flex fixed neon-border justify-center '
+					style={{ left: whereMouse.x + 5, top: whereMouse.y + 20 }}
+					ref={hoverRef}>
+					<Image
+						src={epen.src}
+						width={208}
+						height={208}
+						alt='epen flow'
+						style={{ objectFit: 'contain' }}
+					/>
 				</div>
-				<div className='mt-10 m-2 text-white flex flex-col'>
-					<h1
-						className=''
-						ref={welcomeRef}>
-						~👾Hi mate i think you intrested about my work maybe you
-						want to send me text?
-						<br />
-						send your text in form field below!
-					</h1>
-					<div className='flex flex-col'>
-						{isChange === '' ? null : (
-							<React.Fragment>
-								<div className='w-full overflow-x-hidden overflow-y-auto h-[140px] p-2'>
-									<p className='w-full h-full'>{isChange}</p>
-								</div>
-								<p className='italic'>
-									hit enter to send message!
-								</p>
-							</React.Fragment>
-						)}
-						<div>
-							<label className='uppercase font-bold mr-2'>
-								epenflow©2023~
-							</label>
-							<input
-								type='text'
-								placeholder='Type here'
-								onKeyDown={(el) => handleEnter(el)}
-								className='bg-blue-600 w-1/2 focus:border-none focus:outline-none'
-								onChange={(el) => handleChange(el)}
+				<div
+					className='fixed h-5 w-5  bg-white rounded-full z-50 outline-2 outline-[#39FF14] flex justify-center items-center p-1 neon-border'
+					style={{
+						left: whereMouse.x,
+						top: whereMouse.y + 10,
+					}}>
+					<div className='bg-[#39FF14] rounded-full h-2 w-2'></div>
+				</div>
+
+				<div
+					className='bg-blue-600 w-full h-3/4 md:h-1/2 md:w-1/2 relative rounded-lg border-solid border-2 border-[#39FF14] box-content overflow-y-hidden'
+					ref={terminalRef}>
+					<div className='absolute bg-white top-0 h-7 items-center flex flex-row gap-1  shadow-lg w-full rounded-t-md border-b-2 border-solid border-[#39FF14]'>
+						<Link href={'/works'}>
+							<span
+								className='h-3 w-3 block bg-red-500 rounded-full ml-1 hover:animate-pulse'
+								ref={previusRef}
 							/>
+						</Link>
+						<span className='h-3 w-3 block bg-yellow-500 rounded-full ml-1' />
+						<span className='h-3 w-3 block bg-green-500 rounded-full ml-1' />
+					</div>
+					<div className='mt-10 m-2 text-white flex flex-col'>
+						<h1
+							className=''
+							ref={welcomeRef}>
+							~👾Hi Mate, I think you interested about my work
+							maybe you want to send me a text?
+							<br />
+							send your text in form field below!
+							<br />
+							Click the red button to navigate to the previous
+							page
+						</h1>
+						<div className='flex flex-col'>
+							<div>
+								<label className='uppercase font-bold mr-2'>
+									epenflow©2023~
+								</label>
+								<input
+									type='text'
+									placeholder='Type here...'
+									onKeyDown={(el) => handleEnter(el)}
+									className='bg-blue-600 w-1/2 focus:border-none focus:outline-none placeholder-[#39FF14] placeholder:italic italic animate-pulse'
+									onChange={(el) => handleChange(el)}
+								/>
+							</div>
+							{isChange === '' ? null : (
+								<div className=''>
+									<p className='italic'>
+										hit enter to send message or you can use
+										button send message below!
+									</p>
+									<div className='w-full overflow-x-hidden overflow-y-auto h-[240px]  md:h-[140px] p-2'>
+										<p className='w-full h-full'>
+											{isChange}
+										</p>
+									</div>
+								</div>
+							)}
 						</div>
 					</div>
 				</div>
-			</div>
-		</main>
+				{isChange === '' ? null : (
+					<button
+						onClick={() => handleClick}
+						className='bg-blue-600 p-2 rounded-md neon-border'>
+						<h1>send message</h1>
+					</button>
+				)}
+			</main>
+			<Footer />
+		</React.Fragment>
 	);
 }
