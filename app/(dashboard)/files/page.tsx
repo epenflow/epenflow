22bafb6203571fe38357df5f4def046384da5fd1
epenflow <epenflow@gmail.com>
2024-07-20 23:45:47 +0800

    __delete-dashboard__

--- app/(dashboard)/files/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function FilesPage() {
-	return <div>test</div>;
-}
--- app/(dashboard)/project/page.tsx
+++
@@ -1,5 +0,0 @@
-import db from '@/lib/db';
-
-export default async function ProjectPage() {
-	return <main className="flex w-screen flex-col items-center py-5"></main>;
-}
