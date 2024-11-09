b8993e9fd2e87f23e722e4d701b79e86f1a2085f
epenflow <epenflow@gmail.com>
2024-11-09 23:05:15 +0800

    add format-date as constant

---
+++ src/constants/format-date.constant.ts
@@ -0,0 +1,7 @@
+export const TIME_FH_MM_SS_12H = "KK:MM:SS aa"; /// 01:00:00 AM
+export const TIME_SH_MM_SS_12H = "K:MM:SS aa"; /// 1:00:00 AM
+
+const TIME_UNITS = 60;
+export const ONE_SECOND_IN_MS = 1_000;
+export const ONE_MINUTE_IN_MS = ONE_SECOND_IN_MS * TIME_UNITS;
+export const ONE_HOUR_IN_MS = ONE_MINUTE_IN_MS * TIME_UNITS;
