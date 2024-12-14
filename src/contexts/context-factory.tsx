168b2c7edcd6d8fd190a8b04870f3a0838c4c4dd
epenflow <epenflow@gmail.com>
2024-12-14 14:42:09 +0800

    create context factory

---
+++ src/contexts/context-factory.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type FC<T = Record<string, unknown>> = (
+	props: React.PropsWithChildren<T>
+) => React.ReactNode;
+
+type ContextFactoryReturnType<T> = {
+	Provider: React.MemoExoticComponent<FC>;
+	useContext: () => T;
+};
+function contextFactory<T>(
+	useContextState: () => T,
+	ContextComponent?: React.ReactNode
+): ContextFactoryReturnType<T> {
+	const Context: React.Context<T> = React.createContext({} as T);
+	const RenderContextComponent = React.memo(() => ContextComponent);
+	RenderContextComponent.displayName = "RenderContextComponent";
+
+	const Provider = React.memo<FC>(({ children }) => {
+		return (
+			<Context.Provider value={useContextState()}>
+				{children}
+				<RenderContextComponent />
+			</Context.Provider>
+		);
+	});
+	Provider.displayName = "Provider";
+
+	return {
+		Provider,
+		useContext: () => React.useContext(Context),
+	};
+}
+export default contextFactory;
---
+++ src/test/contexts/context-factory.test.tsx
@@ -0,0 +1,91 @@
+import contextFactory from "@/contexts/context-factory";
+import React from "react";
+import {
+	render,
+	screen,
+	fireEvent,
+	renderHook,
+	cleanup,
+} from "@testing-library/react";
+import { toMatchDiffSnapshot } from "snapshot-diff";
+
+expect.extend({ toMatchDiffSnapshot });
+describe("Context Factory Test", () => {
+	function useMockContextState() {
+		const [isLoading, setLoading] = React.useState<boolean>(false);
+
+		return {
+			isLoading,
+			setLoading,
+		};
+	}
+
+	const { Provider, useContext } = contextFactory(useMockContextState);
+
+	const RenderProvider = (children: React.ReactNode) => {
+		return render(<Provider>{children}</Provider>);
+	};
+
+	const UnitComponent = () => {
+		const { isLoading, setLoading } = useContext();
+		function toggle() {
+			setLoading((prev) => !prev);
+		}
+		return (
+			<main>
+				<h1>Home</h1>
+				<h1>{String(isLoading)}</h1>
+				<button type="button" onClick={toggle}>
+					toggle
+				</button>
+			</main>
+		);
+	};
+
+	afterAll(cleanup);
+
+	it("should render children and provide context value", () => {
+		const { getByRole, getByText } = RenderProvider(<UnitComponent />);
+		expect(getByRole("main")).toBeInTheDocument();
+		expect(getByText(/false/)).toBeInTheDocument();
+	});
+
+	it("should return hook value", () => {
+		const { result } = renderHook(() => useContext(), {
+			wrapper: ({ children }) => <Provider>{children}</Provider>,
+		});
+		expect(result.current).toBeInstanceOf(Object);
+		expect(result.current).toEqual(
+			expect.objectContaining({
+				isLoading: false,
+				setLoading: expect.any(Function),
+			})
+		);
+	});
+	it("should render optional context component if provided", () => {
+		const CustomContextComponent = () => (
+			<span>Test Context Component</span>
+		);
+		const { Provider: CustomProvider } = contextFactory(
+			useMockContextState,
+			<CustomContextComponent />
+		);
+		render(
+			<CustomProvider>
+				<UnitComponent />
+			</CustomProvider>
+		);
+		expect(screen.getByText(/Test Context Component/)).toBeInTheDocument();
+	});
+
+	it("should update the context state", () => {
+		const { getByRole, getByText, asFragment } = RenderProvider(
+			<UnitComponent />
+		);
+		const firstRender = asFragment();
+		expect(getByText(/false/)).toBeInTheDocument();
+		fireEvent.click(getByRole("button"));
+		expect(getByText(/true/)).toBeInTheDocument();
+		expect(firstRender).toMatchDiffSnapshot(asFragment());
+	});
+});
