0d0446313c43a864d1d2863b9f4b2064df4a802c
epenflow <epenflow@gmail.com>
2025-02-21 12:51:29 +0800

    feat : - update the logo - change import to reference - inject global style into main html

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import ReactDOM from 'react-dom/client';
-import Root from '~/components/layout/root';
-import '~/globals.css';
+import ReactDOM from "react-dom/client";
+import Root from "~/components/layout/root";
 
-const root = document.getElementById('root')!;
+const root = document.getElementById("root")!;
 
 if (!root.innerHTML) {
-	ReactDOM.createRoot(root).render(<Root />);
+  ReactDOM.createRoot(root).render(<Root />);
 }
