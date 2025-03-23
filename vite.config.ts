55c4035f1c023a9b9281f5d23f340ac3f1a64b86
epenflow <epenflow@gmail.com>
2025-03-23 15:33:43 +0800

    feat : add target

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
+      target: "react",
       autoCodeSplitting: true,
     }),
     react(),
