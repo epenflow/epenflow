44d1539dbbbcf4b18ca73b9d41b560b287dac399
epenflow <epenflow@gmail.com>
2025-05-29 20:17:59 +0800

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -13,6 +14,7 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    visualizer({ open: true }),
   ],
   resolve: {
     alias: {
@@ -21,5 +23,48 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ["react-scan"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("gsap") || id.includes("@gsap/react")) {
+            return "gsap-vendor";
+          }
+
+          if (id.includes("motion")) {
+            return "motion-vendor";
+          }
+
+          if (
+            id.includes("@tailwindcss/vite") ||
+            id.includes("tailwindcss") ||
+            id.includes("tailwindcss-animate")
+          ) {
+            return "tailwindcss-vendor";
+          }
+
+          if (id.includes("lucide-react")) {
+            return "icon-vendor";
+          }
+
+          if (id.includes("@tanstack/react-router")) {
+            return "router-vendor";
+          }
+
+          if (
+            id.includes("clsx") ||
+            id.includes("tailwind-merge") ||
+            id.includes("class-variance-authority")
+          ) {
+            return "ui-vendor";
+          }
+
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
   },
 });
